39f8f6c5272d83770d9b751386b1a3b7183b43d3
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-06 11:18:43 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const weather = { temperature:25, conditions: "Sunny"};
+  const temperatureColor = weather.temperature > 20 ? 'red' : 'blue';
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+        <h2>Weather Report</h2>
+        <p>Temperature: <span style={{color:temperatureColor}}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
